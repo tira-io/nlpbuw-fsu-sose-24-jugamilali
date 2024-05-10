330c4bff0f10eec8e324c560c732d1b8ccdc0aba
Julian Klüber <135227793+JKlueber@users.noreply.github.com>
2024-05-10 14:07:53 +0000

    debugged labels

--- language-huggingface/run.py
+++ language-huggingface/run.py
@@ -15,20 +15,8 @@ if __name__ == "__main__":
     # Load the model
     model = load(Path(__file__).parent / "model.joblib")
 
-    # Define language IDs and labels mapping
-    lang_ids = [
-        "af", "az", "bg", "cs", "da", "de", "el", "en", "es", "fi", "fr", "hr", "it", "ko", "nl", "no", "pl", "ru", "ur", "zh"
-    ]
-    lang_labels = [
-        "__label__af", "__label__az", "__label__bg", "__label__cs", "__label__da", "__label__de", "__label__el",
-        "__label__en", "__label__es", "__label__fi", "__label__fr", "__label__hr", "__label__it", "__label__ko",
-        "__label__nl", "__label__no", "__label__pl", "__label__ru", "__label__ur", "__label__zh"
-    ]
-    label_mapping = dict(zip(lang_labels, lang_ids))
-
     predictions = model.predict(df["text"])
-    mapped_predictions = [label_mapping[label] for label in predictions]
-    df["lang"] = mapped_predictions
+    df["lang"] = predictions
     df = df[["id", "lang"]]
 
     # Save the predictions
