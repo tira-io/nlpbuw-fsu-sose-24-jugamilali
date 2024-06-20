5b30b8d268c20d9be75790940926b004dfc34973
Julian Klüber <135227793+JKlueber@users.noreply.github.com>
2024-06-20 15:21:38 +0000

    made new summarizing with fine tuned model

--- sum/run.py
+++ sum/run.py
@@ -2,40 +2,40 @@ from pathlib import Path
 from tira.rest_api_client import Client
 from tira.third_party_integrations import get_output_directory
 
-from transformers import pipeline, BartForConditionalGeneration, BartTokenizer
+from transformers import BartForConditionalGeneration, BartTokenizer
 import torch
 
-def summarize_texts(texts, summarizer, max_length=150):
-    summaries = []
-    for text in texts:
-        summary = summarizer(text, max_length=max_length, min_length=40, length_penalty=2.0, num_beams=5, early_stopping=True)
-        summaries.append(summary[0]['summary_text'])
-    return summaries
+# Function to generate summaries
+def generate_summary(batch):
+    inputs = tokenizer(batch['story'], max_length=1024, return_tensors='pt', truncation=True, padding='max_length')
+    inputs = {k: v.to(device) for k, v in inputs.items()}
+    summaries = model.generate(inputs['input_ids'], max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+    batch['summary'] = tokenizer.batch_decode(summaries, skip_special_tokens=True)
+    return batch
 
 if __name__ == "__main__":
     tira = Client()
     df = tira.pd.inputs("nlpbuw-fsu-sose-24", "summarization-validation-20240530-training").set_index("id")
 
     # Determine device
-    device = 0 if torch.cuda.is_available() else -1
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-    model_path = str(Path(__file__).parent / "summarizer_model")
-    tokenizer_path = str(Path(__file__).parent / "summarizer_tokenizer")
+    model_path = str(Path(__file__).parent / "sumbart")
+    tokenizer_path = str(Path(__file__).parent / "sumbart")
 
     model = BartForConditionalGeneration.from_pretrained(model_path)
     tokenizer = BartTokenizer.from_pretrained(tokenizer_path)
 
-    # Load the summarization pipeline
-    summarizer = pipeline("summarization", model=model, tokenizer=tokenizer, device=device)
+    # Move model to the appropriate device
+    model.to(device)
 
-    # Summarize texts
-    df['summary'] = summarize_texts(df['story'], summarizer)
+    # Generate summaries
+    df = df.apply(lambda row: generate_summary(row), axis=1, result_type='expand')
 
     # Prepare the DataFrame for output
     df = df.drop(columns=["story"]).reset_index()
 
     # Save the predictions
     output_directory = get_output_directory(str(Path(__file__).parent))
-    df.to_json(
-        Path(output_directory) / "predictions.jsonl", orient="records", lines=True
-    )
+    output_file = Path(output_directory) / "predictions.jsonl"
+    df.to_json(output_file, orient="records", lines=True)
